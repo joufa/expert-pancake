f0a80a6158b1d037c023ac1c8e6e8430b1627784
Antti Reijonen <areijone@gmail.com>
2016-12-26 23:17:04 +0200

    file

--- file.js
+++ file.js
@@ -3,3 +3,50 @@
  * 
  * TODO: File/string replace operations
  */
+
+var fs = require('fs');
+var fileFolder = 'uploads';
+
+
+function indexDirectory(directory, callback) {
+    fs.readdir(directory, function(err, files) {
+        callback(files);
+    });
+}
+
+var output = indexDirectory(fileFolder, function(data) {
+    console.log(data);
+});
+
+
+// http://stackoverflow.com/questions/6831918/node-js-read-a-text-file-into-an-array-each-line-an-item-in-the-array/34033928#34033928
+/*
+var results = [];
+  var rl = require('readline').createInterface({
+    input: require('fs').createReadStream('./assets/stylesheets/_icons.scss')
+  });
+
+
+  // for every new line, if it matches the regex, add it to an array
+  // this is ugly regex :)
+  rl.on('line', function (line) {
+    var re = /\.icon-icon.*:/;
+    var match;
+    if ((match = re.exec(line)) !== null) {
+      results.push(match[0].replace(".",'').replace(":",''));
+    }
+  });
+
+
+  // readline emits a close event when the file is read.
+  rl.on('close', function(){
+    var outputFilename = './icons.json';
+    fs.writeFile(outputFilename, JSON.stringify(results, null, 2), function(err) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log("JSON saved to " + outputFilename);
+        }
+    });
+  });
+  */
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+/**
+ * TODO AND IDEAS
+ * 
+ * - MongoDB database entries for uploaded files and possible metadata
+ * - UI for searching and combining uploaded entries
+ * - Tool for manually changing the cases
+ * 
+ */
 console.log("unfinished main file");
